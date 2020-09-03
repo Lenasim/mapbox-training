98e24c3f2f72e4275556102bc16ebdf41e022573
LenaSim <radical.rin@gmail.com>
2020-09-03 14:31:59 +0200

    Add  router, link, buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Map from './components/Map';
 import DrivingDirection from './components/DrivingDirection';
 import './App.css';
 
 class App extends React.Component {
   render() {
-    return <DrivingDirection />;
+    return (
+      <Router>
+        <div className='buttons'>
+          <Link to='/map' className='button'>
+            Map
+          </Link>
+          <Link to='/direction' className='button'>
+            Direction
+          </Link>
+        </div>
+        <Switch>
+          <Route path='/map' component={Map} />
+          <Route path='/direction' component={DrivingDirection} />
+        </Switch>
+      </Router>
+    );
   }
 }
 
